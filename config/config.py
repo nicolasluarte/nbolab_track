de3c437258564982661007a93a0ef50728a899ab
nicolasluarte <l.luarte@uc.cl>
2020-12-21 18:47:28 -0300

    update

--- config/config.py
+++ config/config.py
@@ -7,8 +7,8 @@ config['preprocess'] = {
         # should be the same
         # < 10 not much effect
         # > 150 strong effect image look "cartoonish"
-        'sigma_color': '75',
-        'sigma_space': '75'
+        'sigma_color': '25',
+        'sigma_space': '25'
         }
 
 config['postprocess'] = {
