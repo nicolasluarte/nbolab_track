69d7ab5edaab4587f407c0a8f453e6372879ac31
nicolasluarte <l.luarte@uc.cl>
2021-03-22 03:40:28 -0300

    update

--- bin/track2.py
+++ bin/track2.py
@@ -93,7 +93,7 @@ with open(csvPath, 'w') as f:
             centroidX = 0
         if centroidY == 'None':
             centroidY = 0
-        imgJpg = cv2.circle(frame, (int(centroidX), int(centroidY)), radius=10,
+        imgJpg = cv2.circle(framePost, (int(centroidX), int(centroidY)), radius=10,
                             color=(0, 0, 255), thickness=-1)
         cv2.imwrite(previewPath + str(counter) + '.jpg', imgJpg)
         counter = counter + 1
